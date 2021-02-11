7e1e88befc618db232af2c7b91334b9375b36da7
ValeriaVicious <valeriavicious13@gmail.com>
2021-02-11 13:49:20 +0800

    Описание структуры для сохранения позиции игрока

---
+++ Assets/Code/Model/Vector3Serializable.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+
+namespace GeekBrains
+{
+    [Serializable]
+    public struct Vector3Serializable
+    {
+        #region Fields
+
+        public float X;
+        public float Y;
+        public float Z;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        private Vector3Serializable(float valueX, float valueY, float valueZ)
+        {
+            X = valueX;
+            Y = valueY;
+            Z = valueZ;
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public static implicit operator Vector3(Vector3Serializable value)
+        {
+            return new Vector3(value.X, value.Y, value.Z);
+        }
+
+        public static implicit operator Vector3Serializable(Vector3 value)
+        {
+            return new Vector3Serializable(value.x, value.y, value.z);
+        }
+
+        public override string ToString() => $" (X = {X} Y = {Y} Z = {Z})";
+        
+        #endregion
+    }
+}
---
+++ Assets/Code/SavedData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace GeekBrains
+{
+    [Serializable]
+    public sealed class SavedData
+    {
+        #region Fields
+
+        public string Name;
+        public Vector3Serializable Position;
+
+        #endregion
+    }
+}

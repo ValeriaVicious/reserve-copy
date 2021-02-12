a1d48ab9974b63e0600f908d974b43cf293267ff
ValeriaVicious <valeriavicious13@gmail.com>
2021-02-12 16:38:48 +0800

    добавление миникарты

---
+++ Assets/Code/MiniMap.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+
+namespace GeekBrains
+{
+    public sealed class MiniMap : MonoBehaviour
+    {
+        #region Fields
+
+        private Transform _playerPosition;
+        private string _pathOfTexture = "MiniMap/MapTexture";
+        private float _xValue = 90.0f;
+        private float _yValue = 5.0f;
+
+        #endregion
+
+
+        #region UnityMethods
+
+        private void Start()
+        {
+            _playerPosition = Camera.main.transform;
+            transform.parent = null;
+            transform.rotation = Quaternion.Euler(_xValue, 0.0f, 0.0f);
+            transform.position = _playerPosition.position + new Vector3(0.0f, _yValue, 0.0f);
+
+            var renderTexture = Resources.Load<RenderTexture>(_pathOfTexture);
+            GetComponent<Camera>().targetTexture = renderTexture;
+        }
+
+        private void LateUpdate()
+        {
+            var newPosition = _playerPosition.position;
+            newPosition.y = transform.position.y;
+            transform.position = newPosition;
+            transform.rotation = Quaternion.Euler(_xValue, _playerPosition.eulerAngles.y, 0.0f);
+        }
+
+        #endregion
+    }
+}
